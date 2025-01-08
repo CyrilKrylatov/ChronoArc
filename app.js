06d1d823a296e45415a57559f5bcd35a05f65fae
Cyril Krylatov <cyril.krylatov+github@gmail.com>
2025-01-08 21:37:02 +0100

    Switch steps

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 /* global requestAnimationFrame */
 
 /** @type {number} */
-
-const stepsCount = document.querySelector('.js-main').childElementCount
+let activeStep = 0
 
 document.addEventListener('click', ({ target }) => {
-  updateActiveStep()
-  handleSteps(target)
+  updateStape(activeStep + 1)
 })
 
+/**
+ * Update state
+ * @param {Number} step - to go
+ */
 
-function updateActiveStep () {
-  const activeStep = Number(document.body.dataset.activeStep)
-  document.body.dataset.activeStep = activeStep === stepsCount - 1 ? 0 : activeStep + 1
-}
+function updateStape (step) {
+  const stepsElements = document.querySelectorAll('[data-step]')
+  activeStep = step === stepsElements.length ? 0 : step
 
-function handleSteps (element) {
-  console.log(element)
+  const hiddenStep = document.querySelector('[data-step]:not([hidden])')
+  hiddenStep.hidden = true
+
+  const stepElementToDisplay = stepsElements[activeStep]
+  stepElementToDisplay.hidden = false
+
+  if (activeStep === 1) {
+    countDown({
+      seconds: 10,
+      element: stepElementToDisplay,
+      callback: function () {
+        updateStape(2)
+      }
+    })
+  }
+
+  if (activeStep === 2) {
+    countDown({
+      seconds: 120,
+      element: stepElementToDisplay,
+      callback: function () {
+        updateStape(0)
+      }
+    })
+  }
 }
 
 /**
@@ -24,11 +48,12 @@ function handleSteps (element) {
  * with the remaining seconds. Executes a callback function when the timer
  * reaches zero. Uses requestAnimationFrame for better performance and accuracy.
  *
- * @param {number} seconds - The number of seconds for the countdown.
- * @param {HTMLElement} element - The HTML element to display the countdown.
- * @param {Function} callback - The function to execute when the countdown ends.
+ * @param {Object} options
+ * @param {number} options.seconds - The number of seconds for the countdown.
+ * @param {HTMLElement} options.element - The HTML element to display the countdown.
+ * @param {Function} options.callback - The function to execute when the countdown ends.
  */
-function countDown (seconds, element, callback) {
+function countDown ({ seconds, element, callback = () => {} }) {
   if (!element) {
     return
   }
